4a06c916086c3ad589de17de8ee87db6b1d69f39
Vyacheslav Blinov <vblinov@unison.com>
2014-08-14 06:28:02 +0400

    Checkstyle task improvements

--- src/main/scala/codequality/CheckStyle.scala
+++ src/main/scala/codequality/CheckStyle.scala
@@ -1,30 +1,57 @@
 package codequality
 
+import sbt.Keys._
 import sbt._
-import Keys._
 
 object CheckStyle {
-    val checkStyle = TaskKey[Unit]("checkstyle", "run CheckStyle")
-    val checkStyleTask = checkStyle <<=
-        (streams, baseDirectory, sourceDirectory in Compile, target) map {
-            (streams, base, src, target) =>
-                import com.puppycrawl.tools.checkstyle.Main.{ main => CsMain }
+    object ReportFormat extends Enumeration {
+        val plain, xml = Value
+    }
+
+    val format = SettingKey[ReportFormat.Value]("CheckStyle.format", "Output format used by CheckStyle")
+    val outputFile = SettingKey[Option[File]]("CheckStyle.outputFile", "File to print output of CheckStyle")
+    val configFile = SettingKey[File]("CheckStyle.configFile", "CheckStyle configuration file")
+
+    val checkStyle = TaskKey[Int]("checkstyle", "run CheckStyle")
+
+    val defaults = Seq(
+        format := ReportFormat.plain,
+        configFile <<= baseDirectory(_ / "checkstyle-config.xml"),
+        defaultOutputFile,
+        checkStyleTask
+    )
+
+    private def checkStyleTask = checkStyle <<=
+        (streams, sourceDirectory in Compile, format, outputFile, configFile) map {
+            (streams, src, format, outputFile, configFile) =>
+                import com.puppycrawl.tools.checkstyle.Main.{main => runCheckStyle}
                 import streams.log
 
-                val args = List(
-                    "-c", (base / "checkstyle-config.xml").getAbsolutePath,
-                    "-f", "xml",
-                    "-r", src.getAbsolutePath,
-                    "-o", (target / "checkstyle-result.xml").getAbsolutePath
-                )
-                log info ("using checkstyle args " + args)
-                CodeQualityPlugin.trappingExits {
-                    CsMain(args.toArray)
+                if (!configFile.isFile) {
+                    sys.error(s"Config file at ${configFile.getAbsoluteFile} is missing")
                 }
+
+                val outputFileArg = outputFile match {
+                    case Some(file) => "-o" :: file.getAbsolutePath :: Nil
+                    case None => Nil
+                }
+
+                val args = List(
+                    "-c", configFile.getAbsolutePath,
+                    "-f", format.toString,
+                    "-r", src.getAbsolutePath
+                ) ++ outputFileArg
+
+                log debug ("using checkstyle args " + args)
+                CodeQualityPlugin.trappingExits {
+                    runCheckStyle(args.toArray)
+                }.get
         }
 
-    val all = Seq(checkStyleTask)
-    
-    
-//    def run(streams: TaskKey[TaskStreams], base: SettingKey[File], src: SettingKey[File], target: SettingKey[File])
+    private def defaultOutputFile = outputFile <<= (target, format) {
+        (target, format) => format match {
+            case ReportFormat.plain => None
+            case ReportFormat.xml => Some(target / "checkstyle-result.xml")
+        }
+    }
 }
